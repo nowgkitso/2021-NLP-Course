24551a263c7a244423cf4236476596beb7c8087c
nowgkitso <51350420+nowgkitso@users.noreply.github.com>
2021-08-12 00:49:21 +0800

    Update NLP_HW02.py

--- NLP_HW02.py
+++ NLP_HW02.py
@@ -3,11 +3,22 @@ import re
 from bs4 import BeautifulSoup
 import pandas as pd
 # 搜尋目前上映那些電影，擷取出其ID資訊
-for i in range(100):
+title_list = []
+id_list = []
+for i in range(10000):
     url = "https://movies.yahoo.com.tw/movieinfo_main/" + str(i)
     resp = requests.get(url)
     resp.encoding = 'utf-8'
     #print(resp.text)
     soup = BeautifulSoup(resp.text, 'lxml')
     movie_html = soup.find("h1")
-    print(movie_html)
+    print(i)
+    if(movie_html != None):
+        title_list.append(movie_html)
+        id_list.append(i)
+        print(movie_html)
+df_movie_id = pd.DataFrame()
+df_movie_id["電影"]=title_list
+df_movie_id["ID"]=id_list
+#存到檔案做備用
+df_movie_id.to_csv("./movie.csv",encoding="utf-8-sig")
